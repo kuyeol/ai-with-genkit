0b61d46d9dfecb10383e4e1892589b027871f715
dev <125532184+kuyeol@users.noreply.github.com>
2025-04-30 22:43:19 +0900

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,7 @@
   # Sets environment variables in the workspace
   env = {    
     #TODO Get a API key from https://g.co/ai/idxGetGeminiKey 
-    GOOGLE_API_KEY = "AIzaSyCTFY-MBprutyvpjEodSBSBr0DaK4rcJU8"; 
+    GOOGLE_API_KEY = ""; 
   };
   # search for the extension on https://open-vsx.org/ and use "publisher.id"
   idx.extensions = [
